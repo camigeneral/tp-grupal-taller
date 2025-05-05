a83d17f70d1ae29ee1b7f2f0992b537dd9c20956
Camila <camigeneral@gmail.com>
2025-05-05 20:29:59 -0300

    agrego persistencia

--- src/client.rs
+++ src/client.rs
@@ -3,6 +3,7 @@ use std::io::stdin;
 use std::io::Write;
 use std::io::{BufRead, BufReader, Read};
 use std::net::TcpStream;
+use std::thread;
 
 static CLIENT_ARGS: usize = 3;
 
@@ -23,34 +24,54 @@ fn main() -> Result<(), ()> {
     Ok(())
 }
 
+
 fn client_run(address: &str, stream: &mut dyn Read) -> std::io::Result<()> {
     let reader = BufReader::new(stream);
     let mut socket = TcpStream::connect(address)?;
     
-    let mut socket_reader = BufReader::new(socket.try_clone()?);
+    let cloned_socket = socket.try_clone()?;
+    thread::spawn(move || {
+        match listen_to_subscriptions(cloned_socket) {
+            Ok(_) => {
+                println!("Desconectado del nodo");
+            }
+            Err(e) => {
+                eprintln!("Error en la conexión con nodo: {}", e);
+            }
+        }
+    });
 
     for line in reader.lines() {
         if let Ok(line) = line {
             let command = line.trim().to_lowercase();
 
-            if command == "incrementar" || command == "ver" {
+            if command != "salir" {
                 println!("Enviando: {:?}", command);
                 
                 socket.write(command.as_bytes())?;
                 socket.write("\n".as_bytes())?;
-
-                let mut response = String::new();
-                socket_reader.read_line(&mut response)?;
-                println!("Respuesta del servidor: {}", response.trim());
-            } else if command == "salir" {
+            } else {
                 println!("Desconectando del servidor");
                 break;
-            } 
-            
-            else {
-                println!("Comando no reconocido");
             }
         }
     }
+    Ok(())
+}
+
+
+fn listen_to_subscriptions(socket: TcpStream)-> std::io::Result<()> {
+    let mut reader = BufReader::new(socket);
+    loop {
+        let mut response = String::new();
+        let flag = reader.read_line(&mut response)?;
+
+        if flag == 0 {
+            break;
+        }
+
+        println!("{}", response.trim());
+    }
+
     Ok(())
 }
