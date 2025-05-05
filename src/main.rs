262387d6bdf56ffcc4a7b69e3f65ab8bd9e71c76
francoSecchi <francofabiansecchi@gmail.com>
2025-05-05 18:00:03 -0300

    Body dinamico segun los clicks

--- src/main.rs
+++ src/main.rs
@@ -5,12 +5,16 @@ use relm4::{gtk, Component, ComponentParts, ComponentSender, RelmApp, RelmWidget
 
 struct AppModel {
     counter: u8,
+    current_view: String
 }
 
 #[derive(Debug)]
 enum AppMsg {
     Connect,
     Disconnect,
+    ShowHome,
+    ShowDocuments,
+    ShowNewFile,
 }
 
 #[relm4::component]
@@ -38,38 +42,73 @@ impl Component for AppModel {
                     gtk::Button {
                         set_margin_all:10,
                         set_label: "Conectar",
+                        connect_clicked => AppMsg::Connect
                     }
                 },
                 #[name="body_container"]
                 gtk::Box {
-                    set_orientation: gtk::Orientation::Horizontal,
-                    set_hexpand: true,
+                    set_orientation: gtk::Orientation::Horizontal,                
                     set_vexpand: true,
                     #[name="side_bar_container"]
                     gtk::Box {
                         set_orientation: gtk::Orientation::Vertical,
                         set_halign: gtk::Align::Start,
-                        gtk::Box {
-                            set_width_request: 200, 
+                        gtk::Box {                            
                             set_margin_all: 10,
                             set_orientation: gtk::Orientation::Vertical,
                             set_halign: gtk::Align::Center,
                             gtk::Button {
                                 set_margin_all:10,
                                 set_label: "Home",
+                                connect_clicked => AppMsg::ShowHome
                             },
                             gtk::Button {
                                 set_margin_all:10,
                                 set_label: "Mis documentos",
+                                connect_clicked => AppMsg::ShowDocuments
                             },
                             
                             gtk::Button {
                                 set_margin_all:10,
                                 set_margin_top: 50,
                                 set_label: "Nuevo Archivo",
+                                connect_clicked => AppMsg::ShowNewFile
                             }
                         }
-                    },                    
+},                    
+                    #[name="body"]
+                    gtk::Box {
+                        set_hexpand: true,
+                        set_valign: gtk::Align::Start,
+                        set_orientation: gtk::Orientation::Vertical,
+                        set_halign: gtk::Align::Center,
+                        match model.current_view.as_str()  {
+                            "Home" => {
+                                gtk::Label {
+                                    set_text: "Bienvenido a Home!",
+                                    set_margin_all: 20,
+                                }
+                            }
+                            "Documents" => {
+                                gtk::Label {
+                                    set_text: "Aquí están tus documentos.",
+                                    set_margin_all: 20,
+                                }
+                            }
+                            "NewFile" => {
+                                gtk::Label {
+                                    set_text: "Crea un nuevo archivo.",
+                                    set_margin_all: 20,
+                                }
+                            }
+                            _ => {
+                                gtk::Label {
+                                    set_text: "Selecciona una opción del menú.",
+                                    set_margin_all: 20,
+                                }
+                            }
+                        }
+                    }
 
                 }
 
@@ -83,12 +122,22 @@ impl Component for AppModel {
         root: Self::Root,
         _sender: ComponentSender<Self>,
     ) -> ComponentParts<Self> {
-        let model = AppModel { counter };
+        let model = AppModel { counter, current_view: "Home".to_string() };
 
         // Insert the macro code generation here
         let widgets = view_output!();
 
         ComponentParts { model, widgets }
+    }
+
+    fn update(&mut self, message: Self::Input, _sender: ComponentSender<Self>, _root: &Self::Root) {
+        match message {
+            AppMsg::ShowHome => self.current_view = "Home".to_string(),
+            AppMsg::ShowNewFile => self.current_view = "NewFile".to_string(),
+            AppMsg::ShowDocuments => self.current_view = "Documents".to_string(),
+            AppMsg::Connect => self.current_view = "Documents".to_string(),
+            AppMsg::Disconnect => self.current_view = "Documents".to_string()
+        }
     }
 
     fn update_cmd(
