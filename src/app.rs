0c246d317c2d8161faf2ea32de9d1fe3e68af4b8
francoSecchi <francofabiansecchi@gmail.com>
2025-05-14 18:03:57 -0300

    integracion con login

--- src/app.rs
+++ src/app.rs
@@ -1,11 +1,14 @@
 extern crate gtk4;
 extern crate relm4;
+use crate::components::login::{LoginForm, LoginOutput};
+
 use self::gtk4::{
     prelude::{BoxExt, GtkWindowExt, OrientableExt, WidgetExt},
     CssProvider,
 };
 use components::file_workspace::FileWorkspace;
 use components::header::{NavbarModel, NavbarMsg, NavbarOutput};
+use std::collections::HashMap;
 
 use self::relm4::{
     gtk, Component, ComponentController, ComponentParts, ComponentSender, Controller,
@@ -17,17 +20,22 @@ use self::relm4::{
 /// # Campos
 /// - `header_cont`: Controlador para la barra de navegación superior
 /// - `files_manager_cont`: Controlador para el área de gestión de archivos
+/// - `login_form_cont`: Controlador para el formulario de login
+/// - `is_logged_in`: Indica si el usuario ha iniciado sesión
 pub struct AppModel {
     header_cont: Controller<NavbarModel>,
     files_manager_cont: Controller<FileWorkspace>,
+    login_form_cont: Controller<LoginForm>,
+    is_logged_in: bool,
 }
 
 #[derive(Debug)]
 pub enum AppMsg {
-    /// Establece conexión con el server
     Connect,
-    /// Mensaje vacío (no operation)
     Ignore,
+    LoginSuccess(String),
+    LoginFailure(String),
+    Logout,
 }
 
 #[relm4::component(pub)]
@@ -43,16 +51,34 @@ impl SimpleComponent for AppModel {
         set_default_height: 600,
         #[wrap(Some)]
         set_titlebar = model.header_cont.widget(),
+        
         #[name="main_container"]
         gtk::Box {
             set_orientation: gtk::Orientation::Vertical,
             set_spacing: 5,
             set_margin_all: 10,
             set_hexpand: true,
-            set_vexpand: true,
-            append: model.files_manager_cont.widget()
-            }
-        }
+            set_vexpand: true,            
+            gtk::Box {                
+                append: model.files_manager_cont.widget(), 
+                #[watch]
+                set_visible: model.is_logged_in     
+            },
+
+            gtk::Box {
+                set_orientation: gtk::Orientation::Vertical,
+                set_halign: gtk::Align::Center,
+                set_valign: gtk::Align::Center,        
+                set_hexpand: true,
+                set_vexpand: true,        
+                append: model.login_form_cont.widget(),                                    
+                #[watch]
+                set_visible: !model.is_logged_in
+            },    
+                                         
+        },                                             
+        
+    }
     }
 
     fn init(
@@ -69,18 +95,36 @@ impl SimpleComponent for AppModel {
             gtk4::STYLE_PROVIDER_PRIORITY_APPLICATION,
         );
 
+        let mut users = HashMap::new();
+        users.insert("fran".to_string(), "123123".to_string());
+        users.insert("cami".to_string(), "123123".to_string());
+        users.insert("valen".to_string(), "123123".to_string());
+        users.insert("rama".to_string(), "123123".to_string());        
+
         let header_model = NavbarModel::builder().launch(()).forward(
             sender.input_sender(),
-            |_: NavbarOutput| AppMsg::Connect,
+            |output| match output {
+                NavbarOutput::ToggleConnectionRequested => AppMsg::Connect,            
+            },
         );
+        
         let files_manager_model = FileWorkspace::builder().launch(()).forward(
             sender.input_sender(),
             |_: ()| AppMsg::Ignore,
+        );
+        
+        let login_form_model = LoginForm::builder().launch(users).forward(
+            sender.input_sender(),
+            |output| match output {
+                LoginOutput::LoginSuccess(username) => AppMsg::LoginSuccess(username),               
+            },
         );
 
         let model = AppModel {
             header_cont: header_model,
             files_manager_cont: files_manager_model,
+            login_form_cont: login_form_model,
+            is_logged_in: false,
         };
 
         let widgets = view_output!();
@@ -91,12 +135,41 @@ impl SimpleComponent for AppModel {
     fn update(&mut self, message: Self::Input, _sender: ComponentSender<Self>) {
         match message {
             AppMsg::Connect => {
+                if self.is_logged_in {
+                    self.header_cont
+                        .sender()
+                        .send(NavbarMsg::SetConnectionStatus(true))
+                        .unwrap();
+                }
+            }
+            AppMsg::Ignore => {}
+            AppMsg::LoginSuccess(username) => {                
+                self.header_cont
+                    .sender()
+                    .send(NavbarMsg::SetLoggedInUser(username))
+                    .unwrap();
+                
                 self.header_cont
                     .sender()
                     .send(NavbarMsg::SetConnectionStatus(true))
                     .unwrap();
+                self.is_logged_in = true;                        
             }
-            AppMsg::Ignore => {}
+            AppMsg::LoginFailure(_error) => {
+            }
+            AppMsg::Logout => {
+                self.header_cont
+                    .sender()
+                    .send(NavbarMsg::SetConnectionStatus(false))
+                    .unwrap();
+                
+                self.header_cont
+                    .sender()
+                    .send(NavbarMsg::SetLoggedInUser("".to_string()))
+                    .unwrap();
+
+                self.is_logged_in = false;
+            }
         }
     }
 }
