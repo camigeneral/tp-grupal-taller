f588dce04cf68b96fc32f63597cd73c6cffd446a
francoSecchi <francofabiansecchi@gmail.com>
2025-05-19 01:57:45 -0300

    modularizaicon

--- src/lib.rs
+++ src/lib.rs
@@ -1,11 +1,6 @@
-pub mod redis_commands;
 pub mod parse;
 pub mod client_info;
-pub mod redis_response;
-pub mod list_commands;
-pub mod set_commands;
-pub mod string_commands;
-pub mod pub_sub_commands;
+
 
 /// Módulo principal de la aplicación.
 ///
