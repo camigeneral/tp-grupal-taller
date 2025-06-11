f5c7dcba4d0b0718306e4cd07d866949fa8bdea6
valenmdr <vmdr18@gmail.com>
2025-06-11 14:47:33 +0000

    Mejor visualizacion del archivo

--- src/utils/logger.rs
+++ src/utils/logger.rs
@@ -1,10 +1,17 @@
 use std::fs::OpenOptions;
 use std::io::Write;
-use std::time::{SystemTime, UNIX_EPOCH};
+
+extern crate chrono;
+use self::chrono::Local;
+
+pub fn get_readable_datetime() -> String {
+    let now = Local::now();
+    format!("[{}]", now.format("%Y-%m-%d %H:%M:%S"))
+}
 
 pub fn log_event(log_path: &str, message: &str) {
-    let now = SystemTime::now().duration_since(UNIX_EPOCH).unwrap();
-    let log_line = format!("[{}] {}\n", now.as_secs(), message);
+    let datetime = get_readable_datetime();
+    let log_line = format!("{} {}\n", datetime, message);
 
     if let Ok(mut file) = OpenOptions::new().create(true).append(true).open(log_path) {
         let _ = file.write_all(log_line.as_bytes());
