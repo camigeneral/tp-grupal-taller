11d9e7415eb56b7394fc7e2259ada0e391d1a983
francoSecchi <francofabiansecchi@gmail.com>
2025-06-23 04:14:40 -0300

    parteql

--- src/utils/redis_parser.rs
+++ src/utils/redis_parser.rs
@@ -21,7 +21,7 @@ use std::thread;
 /// - `Null`: representa un valor nulo (`$-1`)
 /// - `Error`: mensaje de error
 /// - `Array`: lista de valores anidados
-#[derive(Debug, Clone)]
+#[derive(Debug, Clone, PartialEq)]
 #[allow(dead_code)]
 pub enum ValueType {
     Integer(i64),
