3ae00c3bb11e7420b6b43b4273e2c4d1e305b0e9
francoSecchi <francofabiansecchi@gmail.com>
2025-06-23 15:25:04 -0300

    se elimina el cerrar sesion

--- src/components/header.rs
+++ src/components/header.rs
@@ -1,6 +1,6 @@
 extern crate gtk4;
 extern crate relm4;
-use self::gtk4::prelude::{ButtonExt, PopoverExt, WidgetExt};
+use self::gtk4::prelude::{PopoverExt, WidgetExt};
 use self::relm4::{gtk, ComponentParts, ComponentSender, SimpleComponent};
 
 /// Modelo que representa la barra de navegación (navbar). Gestiona el estado de la conexión y
@@ -66,23 +66,13 @@ impl SimpleComponent for NavbarModel {
                 #[watch]
                 set_visible: model.is_connected,
             },
-
-            pack_end = &gtk::Button {
-                #[watch]
-                set_label: &(if model.is_connected { "Cerrar sesión" } else { "Conectarse" }),
-                connect_clicked[sender] => move |_| {
-                    sender.output(NavbarOutput::ToggleConnectionRequested).unwrap();
-                },
-                #[watch]
-                set_visible: model.is_connected,
-            },
          },
     }
 
     fn init(
         _init: Self::Init,
         root: Self::Root,
-        sender: ComponentSender<Self>,
+        _sender: ComponentSender<Self>,
     ) -> ComponentParts<Self> {
         let model = NavbarModel {
             is_connected: false,
