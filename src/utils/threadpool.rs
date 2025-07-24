f01dd245db3f891fe393123e44cc6300d918c231
francoSecchi <francofabiansecchi@gmail.com>
2025-07-24 02:55:29 -0300

    threadpool basico structs

---
+++ src/utils/threadpool.rs
@@ -0,0 +1,30 @@
+use std::sync::{Arc, Mutex};
+use std::thread;
+use std::sync::mpsc;
+use std::collections::VecDeque;
+
+struct ThreadPool {
+    workers: Vec<Worker>,
+    sender: mpsc::Sender<Job>,
+}
+
+type Job = Box<dyn FnOnce() + Send + 'static>;
+
+impl ThreadPool {
+    fn new(size: usize) -> ThreadPool {
+        let (sender, receiver) = mpsc::channel();
+        let receiver = Arc::new(Mutex::new(receiver));
+        let mut workers = Vec::with_capacity(size);
+        for id in 0..size {
+            workers.push(Worker::new(id, Arc::clone(&receiver)));
+        }
+        ThreadPool { workers, sender }
+    }
+    fn execute<F>(&self, f: F)
+    where
+        F: FnOnce() + Send + 'static,
+    {
+        let job = Box::new(f);
+        self.sender.send(job).unwrap();
+    }
+}
