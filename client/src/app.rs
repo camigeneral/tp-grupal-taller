e768cca71598cbf86852123e8ff3b987e879d215
francoSecchi <francofabiansecchi@gmail.com>
2025-08-03 01:09:57 -0300

    codigo duplicado

--- client/src/app.rs
+++ client/src/app.rs
@@ -646,11 +646,6 @@ impl SimpleComponent for AppModel {
                 self.command = format!("client-llm-response|{}|{}", resp_command[0], resp_command_str);
                 sender.input(AppMsg::ExecuteCommand);
             }
-            AppMsg::PublishLlmResponse(resp_command) => {
-                let resp_command_str = resp_command.iter().map(|s| s.to_string()).collect::<Vec<String>>().join("|");
-                self.command = format!("client-llm-response|{}|{}", resp_command[0], resp_command_str);
-                sender.input(AppMsg::ExecuteCommand);
-            }
         }
     }
 }
