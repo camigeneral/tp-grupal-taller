08cdde23e89cebeea7fdcbedf6da347e295d5b9e
francoSecchi <francofabiansecchi@gmail.com>
2025-08-07 23:51:37 -0300

    agregando logs

--- clean_rdb_files.sh
+++ clean_rdb_files.sh
@@ -1,15 +1,12 @@
-FILES=(
-  "redis_server/rdb_files/redis_node_0_5460_14000.rdb"
-  "redis_server/rdb_files/redis_node_5460_10921_14001.rdb"
-  "redis_server/rdb_files/redis_node_10921_16383_14002.rdb"
+#!/bin/bash
+
+FILES=("redis_server/rdb_files/redis_node_0_5460_14000.rdb" "redis_server/rdb_files/redis_node_5460_10921_14001.rdb" "redis_server/rdb_files/redis_node_10921_16383_14002.rdb"
   "redis_server/rdb_files/redis_node_0_5460_14003.rdb"
   "redis_server/rdb_files/redis_node_0_5460_14004.rdb"
   "redis_server/rdb_files/redis_node_5460_10921_14005.rdb"
   "redis_server/rdb_files/redis_node_5460_10921_14006.rdb"
   "redis_server/rdb_files/redis_node_10921_16383_14007.rdb"
   "redis_server/rdb_files/redis_node_10921_16383_14008.rdb"
-  "logs/llm_microservice.log"
-  "logs/microservice.log/"
   "logs/node0.log"
   "logs/node1.log"
   "logs/node2.log"
@@ -19,7 +16,8 @@ FILES=(
   "logs/node6.log"
   "logs/node7.log"
   "logs/node8.log"
-)
+  "logs/llm_microservice.log"
+  "logs/microservice.log")
 
 for file in "${FILES[@]}"; do
     if [ -f "$file" ]; then
